3df587a10a892e31651355b62855398a17a0dd73
Brendan Jackman <bhenryj0117@gmail.com>
2025-04-12 14:45:21 +0200

    Update trustedUsers setting

--- configuration.nix
+++ configuration.nix
@@ -33,7 +33,7 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINI/kH+QS+z6PrwR/MqRlbUklUowEZiDPwpyMa+6Kb9k jackmanb@jackmanb01"
     ];
   };
-  nix.trustedUsers = [ "root" "@wheel" "brendan" ];
+  nix.settings.trustedUsers = [ "root" "@wheel" "brendan" ];
   security.sudo.wheelNeedsPassword = false;
 
   environment.systemPackages = with pkgs; [
