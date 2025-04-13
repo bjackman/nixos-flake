3196a1c0d44292ba5cd05ecdda9623991053daf2
Brendan Jackman <bhenryj0117@gmail.com>
2025-04-13 23:42:27 +0200

    Add nixfmt and apply it

--- flake.nix
+++ flake.nix
@@ -1,7 +1,5 @@
 {
-  inputs = {
-    nixpkgs = { url = "github:nixos/nixpkgs/nixos-24.11"; };
-  };
+  inputs = { nixpkgs = { url = "github:nixos/nixpkgs/nixos-24.11"; }; };
 
   outputs = inputs@{ self, nixpkgs }: {
     nixosConfigurations = {
@@ -29,9 +27,8 @@
     # For this not to be tied to x86 you should use something like flake-utils
     # which provides more wrappers, which lets you make this architecture
     # agnostic.
-    devShells.x86_64-linux.default = let pkgs = import nixpkgs {
-      system = "x86_64-linux";
-    };
-    in pkgs.mkShell { packages = [ pkgs.nil ]; };
+    devShells.x86_64-linux.default =
+      let pkgs = import nixpkgs { system = "x86_64-linux"; };
+      in pkgs.mkShell { packages = with pkgs; [ nil nixfmt ]; };
   };
 }
