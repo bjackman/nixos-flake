3b3239086d832317deff40b37616fae0001cffec
Brendan Jackman <bhenryj0117@gmail.com>
2025-04-14 21:39:22 +0200

    Move common.nix content into explicit config field
    IIUC you can do this implicitly, but looking at some examples, it
    seems like I might want to set replaceStdenv in order to apply
    ccache. And that seems like it might live outside of .config. So,
    make it explicit.

--- common.nix
+++ common.nix
@@ -1,41 +1,43 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  boot = {
-    loader = {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
+  config = {
+    boot = {
+      loader = {
+        systemd-boot.enable = true;
+        efi.canTouchEfiVariables = true;
+      };
+      kernelPackages = pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor
+        (pkgs.buildLinux {
+          version = "6.14";
+          src = pkgs.fetchgit {
+            url = "https://github.com/torvalds/linux.git";
+            rev = "v6.14";
+            hash = "sha256-5Fkx6y9eEKuQVbDkYh3lxFQrXCK4QJkAZcIabj0q/YQ=";
+          };
+        }));
     };
-    kernelPackages = pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor
-      (pkgs.buildLinux {
-        version = "6.14";
-        src = pkgs.fetchgit {
-          url = "https://github.com/torvalds/linux.git";
-          rev = "v6.14";
-          hash = "sha256-5Fkx6y9eEKuQVbDkYh3lxFQrXCK4QJkAZcIabj0q/YQ=";
-        };
-      }));
+
+    time.timeZone = "Europe/Zurich";
+    i18n.defaultLocale = "en_US.UTF-8";
+
+    users.users.brendan = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      packages = with pkgs; [ tree ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINI/kH+QS+z6PrwR/MqRlbUklUowEZiDPwpyMa+6Kb9k jackmanb@jackmanb01"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMaakNfELyvjLLCRwH2U/yQ35HkEW+hEShAD7sn0mCmH brendan@chungito"
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDLVpV3PnFV5AW4G0aizNgoVu0Wtn3A3arUEJHaEsxy3iFgvvENBcYb+I00HRnYV4FZX1EGD0Fh6lIJcm9YUCm2EKkv9V/mMfV5xaiKcKGZYOLLpaIZw8J3tsuc+iIrl/8Qk1++l6pYIgOCpAgRAY1MxSD/Syg7rZMKiIH2/3CAzzjQej3SCf0Wc2I2/Sv1YUUhNxKGkMi7P4lG8R2erRG8DuPsglEhHW0ua3Hkygy3lfBO9j32JdOXB6+xswWOljiUwnVMt4AbBrZPxn/29BlS/olEgdfxt+jBNM33h9ofKwM+h5oGXomNedgr9qQVha4xj+dbqD7YB/lB/9HMjd1X jackmanb@jackmanb.zrh.corp.google.com"
+      ];
+    };
+    nix.settings.trusted-users = [ "root" "@wheel" "brendan" ];
+    security.sudo.wheelNeedsPassword = false;
+
+    environment.systemPackages = with pkgs; [ vim wget neofetch ];
+    services.openssh.enable = true;
+    networking.firewall.enable = false;
+
+    system.stateVersion = "24.11"; # DO NOT CHANGE IT! NEVER CHANGE IT!
   };
-
-  time.timeZone = "Europe/Zurich";
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  users.users.brendan = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    packages = with pkgs; [ tree ];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINI/kH+QS+z6PrwR/MqRlbUklUowEZiDPwpyMa+6Kb9k jackmanb@jackmanb01"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMaakNfELyvjLLCRwH2U/yQ35HkEW+hEShAD7sn0mCmH brendan@chungito"
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDLVpV3PnFV5AW4G0aizNgoVu0Wtn3A3arUEJHaEsxy3iFgvvENBcYb+I00HRnYV4FZX1EGD0Fh6lIJcm9YUCm2EKkv9V/mMfV5xaiKcKGZYOLLpaIZw8J3tsuc+iIrl/8Qk1++l6pYIgOCpAgRAY1MxSD/Syg7rZMKiIH2/3CAzzjQej3SCf0Wc2I2/Sv1YUUhNxKGkMi7P4lG8R2erRG8DuPsglEhHW0ua3Hkygy3lfBO9j32JdOXB6+xswWOljiUwnVMt4AbBrZPxn/29BlS/olEgdfxt+jBNM33h9ofKwM+h5oGXomNedgr9qQVha4xj+dbqD7YB/lB/9HMjd1X jackmanb@jackmanb.zrh.corp.google.com"
-    ];
-  };
-  nix.settings.trusted-users = [ "root" "@wheel" "brendan" ];
-  security.sudo.wheelNeedsPassword = false;
-
-  environment.systemPackages = with pkgs; [ vim wget neofetch ];
-  services.openssh.enable = true;
-  networking.firewall.enable = false;
-
-  system.stateVersion = "24.11"; # DO NOT CHANGE IT! NEVER CHANGE IT!
 }
