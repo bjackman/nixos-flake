5cb859320d89f7a944d12a37f20809f8c83cbf09
Brendan Jackman <bhenryj0117@gmail.com>
2025-04-14 21:41:41 +0200

    Add missing qemu.nix

---
+++ qemu.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports = [ "${modulesPath}/virtualisation/qemu-vm.nix" ];
+
+  virtualisation = {
+    forwardPorts = [{
+      from = "host";
+      host.port = 2222;
+      guest.port = 22;
+    }];
+    graphics = false;
+  };
+}
