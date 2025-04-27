1f5524db6803881f9c309597f14d2976bcfe16ed
Brendan Jackman <bhenryj0117@gmail.com>
2025-04-27 14:32:34 +0200

    Delete comment about nixos-wrapper app definition
    Now I understand things a bit better I actually think this is pretty
    reasonable, I dunno. I'm sure there's a better way to do it but this
    isn't actually completely insane.

--- flake.nix
+++ flake.nix
@@ -97,12 +97,6 @@
 
       apps.x86_64-linux.nixos-rebuild = {
         type = "app";
-        # writeShellApplication was suggested by AI. It returns a derivation.
-        # But .program needs to be a store path. AI suggested that I just
-        # interpolate it into a string and append the binary path onto the end
-        # of it. This is stupid as hell and definitely not how you're supposed
-        # to do this. Examples I found on GitHub seem to not have this problem.
-        # I dunno.
         program = "${
             pkgs.writeShellApplication {
               name = "nixos-rebuild-wrapper";
