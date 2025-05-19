e577f29ecdf734a2e6f59e7294b50b3a881855b9
Brendan Jackman <jackmanb@google.com>
2025-05-19 14:26:16 +0000

    Reduce grub timeout

--- modules/common.nix
+++ modules/common.nix
@@ -3,4 +3,6 @@
   # I don't really understand this, how to best set it for new installs is a
   # mystery to me.
   system.stateVersion = "24.11";
+
+  boot.loader.timeout = 2; # enspeeden tha boot
 }
