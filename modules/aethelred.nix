3840768bf6d638bac09f0b278c883d236e82612e
Brendan Jackman <jackmanb@google.com>
2025-05-23 15:24:42 +0000

    aethelred: Drop networking customization
    Not needed any more, it's just a normal corned puter on a normal DHCP
    type network.

--- modules/aethelred.nix
+++ modules/aethelred.nix
@@ -1,12 +1,5 @@
 { config, lib, pkgs, ... }:
 {
   imports = [ ./aethelred-hardware-configuration.nix ];
-  networking = {
-    hostName = "aethelred";
-    interfaces.eno2.useDHCP = true;
-    interfaces.eno1.ipv4.addresses = [{
-      address = "192.168.2.3";
-      prefixLength = 24;
-    }];
-  };
+  networking.hostName = "aethelred";
 }
