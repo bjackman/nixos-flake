4215fe41b16cdfc214f4bbfbfb46e483d15dfe46
Brendan Jackman <jackmanb@google.com>
2025-06-19 11:32:26 +0000

    Enable flakes in target
    This will be handy for running Nix builds as a simple benchmark

--- modules/brendan.nix
+++ modules/brendan.nix
@@ -14,7 +14,12 @@
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDMi2QRJG+/nM2ekysSUT6h1uNlSmo31ubSK28DrGVezoh2MaPXz6XWMpJtDvr9FHHOVpsCTFxFQ9A7DTqgFy0NxwTHJhK5bevxaWYRkv43H8EMR9pJXYMDAtj7Gk+NNK5ssGZm2P+cTl9r5QZOm0PaVUUeoA/KxbVCNEenOCHM5Lv2RrXGufJL1ukRL6I83fl3ilfgEOz2RBG3QQGahVqYfZq/mfo07U0vad9RX7y6I+8Ap8XSCe33yfO0338yPf0A69p90xtpiJyYyAtVN+0KT552wpMtPjprXt5mrpYDLZvW6vBu0mFGkmDoz3ekb+MmWJVlE9f1VyjHpmA1bRn18gQ73egrGlVWvPHpAJ3gl5bKtc30Md/M4u3tyauDoAnqOs/FAqvClDz1Yav+5Ck5umnDSXXWH/WToX9AUsevjLQq1uB2QJU6oYeEIpEHWC4dUtgPXrX/SYDSGmqA5xOqboyn39oIcNWXTOrqnes52bBlOW3/zCX51EIx/tiG3LU= brendan@brendan-thinkpad"
     ];
   };
-  nix.settings.trusted-users = [ "root" "@wheel" "brendan" ];
+
+  nix = {
+    settings.trusted-users = [ "root" "@wheel" "brendan" ];
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
+
   security.sudo.wheelNeedsPassword = false;
 
   environment.systemPackages = with pkgs; [ vim file ];
