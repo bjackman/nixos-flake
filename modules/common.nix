8eab718c1928d79b6e56447320e7c9fd31053386
Brendan Jackman <jackmanb@google.com>
2025-06-20 14:05:47 +0000

    Move VM memory and CPU config to common.nix
    This is needed so that the "guest" config is big enough to run
    benchmarks.

--- modules/common.nix
+++ modules/common.nix
@@ -9,4 +9,12 @@
 
     tmp.useTmpfs = true;
   };
+
+  virtualisation.vmVariant.virtualisation = {
+    # Approximate actual size of Aethelred (`free -m`). This doesn't actually
+    # result in the exact amount of RAM being available though, maybe because of
+    # BIOS type changes.
+    memorySize = 63817;
+    cores = 12;  # This is presumably actually logical CPUs.
+  };
 }
